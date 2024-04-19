08dfdb046590830fab7d0c4a613e78c567c158e8
Roberto Costa <robertocosta66@gmail.com>
2024-04-19 11:40:52 -0300

    OpenAPIConfig

---
+++ src/main/java/com/devsuperior/dsmovie/config/OpenAPIConfig.java
@@ -0,0 +1,26 @@
+package com.devsuperior.dsmovie.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+
+@OpenAPIDefinition
+@Configuration
+public class OpenAPIConfig {
+
+	@Bean
+	public OpenAPI dsmovieAPI() {
+		return new OpenAPI()
+				.info(new Info()
+				.title("DSMovie API")
+				.description("DSMovie Reference Project")
+				.version("v0.0.1")
+				.license(new License()
+				.name("Apache 2.0")
+				.url("https://github.com/robcosta/dsmovie-documentation-api")));
+	}
+}
